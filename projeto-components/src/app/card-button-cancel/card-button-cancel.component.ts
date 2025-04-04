2714c2e954307468d9e595bf116afeef346e0c28
Euclydes Uchoas Dos Santos Neto <tiddys08@gmail.com>
2025-04-03 22:03:13 -0300

    feat: entendimento do template, templateUrl, styles, styleUrl, styleUrl, dentre outras funcionalidades

---
+++ projeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-button-cancel',
+  standalone: false,
+  templateUrl: './card-button-cancel.component.html',
+  styleUrls: [
+    './card-button-cancel.component.scss',
+    '../card/card.component.scss'
+  ]
+})
+export class CardButtonCancelComponent {
+
+}
