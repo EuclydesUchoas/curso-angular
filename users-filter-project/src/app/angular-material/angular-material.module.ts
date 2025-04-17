12db94d728a620cb6711fc54dbe31f73577b2413
Euclydes Uchoas Dos Santos Neto <tiddys08@gmail.com>
2025-04-16 22:10:47 -0300

    feat: users-filter-project: Componente de filtro.

--- users-filter-project/src/app/angular-material/angular-material.module.ts
+++ users-filter-project/src/app/angular-material/angular-material.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { /*MatNativeDateModule, */provideNativeDateAdapter } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
     imports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        //MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule
     ],
     exports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        //MatNativeDateModule,
+        MatSelectModule,
+        MatButtonModule
+    ],
+    providers: [
+        provideNativeDateAdapter()
     ]
 })
 export class AngularMaterialModule { }
