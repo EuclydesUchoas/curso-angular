095cb9e03be1ff93874340aeb9bf9499e88b5d09
Euclydes Uchoas Dos Santos Neto <tiddys08@gmail.com>
2025-04-17 21:09:31 -0300

    feat: users-filter-project: Pipe de telefone.

---
+++ users-filter-project/src/app/pipes/pipes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { PhonePipe } from './phone.pipe';
+
+@NgModule({
+    declarations: [
+        PhonePipe
+    ],
+    imports: [],
+    exports: [
+        PhonePipe
+    ]
+})
+export class PipesModule { }
