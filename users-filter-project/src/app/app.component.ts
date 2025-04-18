4a3a8a20b8e201ff0ac9e6f09fccb6f008f4795a
Euclydes Uchoas Dos Santos Neto <tiddys08@gmail.com>
2025-04-17 22:56:33 -0300

    feat: users-filter-project: Passagem das lista de usuários via @Input.

--- users-filter-project/src/app/app.component.ts
+++ users-filter-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,16 @@ import { IUser } from './interfaces/user/user.interface';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  usersList: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.usersList = UsersList;
+    }, 3000);
+  }
 
   onUserSelected(user: IUser) {
     this.userSelected = user;
