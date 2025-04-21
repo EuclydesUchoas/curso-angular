d12911e434500003237bc9c7a595a66a307b923e
Euclydes Uchoas Dos Santos Neto <tiddys08@gmail.com>
2025-04-21 19:03:40 -0300

    feat: diretivas-de-atributos: Utilizando o @HostListener.

---
+++ diretivas-de-atributos/src/app/directives/listener.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, HostListener } from "@angular/core";
+
+@Directive({
+    selector: "[appListener]",
+    standalone: false
+})
+export class ListenerDirective {
+    @HostListener('click')
+    onClick() {
+        console.log('CLICOU!');
+    }
+
+    @HostListener('keyup', ['$event', '"Meu Argumento"'])
+    onKeyUp(event: Event, param2: string) {
+        //console.log('DIGITOU!', event);
+        console.log(param2);
+        
+        const fullText = (event.target as HTMLInputElement).value;
+
+        console.log(fullText);
+    }
+}
