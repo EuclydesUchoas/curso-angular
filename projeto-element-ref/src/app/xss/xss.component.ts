7a315cb6c92b3e836dcc5c3e9451dbdeaea6ef6a
Euclydes Uchoas Dos Santos Neto <tiddys08@gmail.com>
2025-04-22 22:08:42 -0300

    feat: projeto-element-ref: Entendendo o problema de XSS com o ElementRef.

---
+++ projeto-element-ref/src/app/xss/xss.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-xss',
+  standalone: false,
+  templateUrl: './xss.component.html',
+  styleUrl: './xss.component.scss'
+})
+export class XssComponent {
+  constructor(
+    private readonly _elRef: ElementRef
+  ) { }
+  createElement(inputText: string) {
+    const divEl = document.createElement('div');
+
+    divEl.innerHTML = inputText;
+
+    this._elRef.nativeElement.appendChild(divEl);
+  }
+}
